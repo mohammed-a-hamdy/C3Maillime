3b7464785c326fa5cf1fb6a1fd6c2dba317d30c6
mohammed-a-hamdy <heroboy107@gmail.com>
2025-03-18 01:25:06 +0200

    first commit

---
+++ tests/test-Widget.spec.ts
@@ -0,0 +1,26 @@
+// tests/test-1.spec.ts
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+const baseURL = 'file:///D:/Upwork/C3Maillime/widgetWrapper.html';
+
+test('Send a message', async ({ page }) => {
+  const randomMessage = faker.lorem.sentence();
+  await page.goto(baseURL);
+  await page.getByRole('button', { name: 'Open chat window' }).click();
+  await expect(page.locator('#chatwoot_live_chat_widget').contentFrame().getByRole('button')).toContainText('Start Conversation');
+  await page.locator('#chatwoot_live_chat_widget').contentFrame().getByRole('button', { name: 'Start Conversation' }).click();
+  await page.locator('#chatwoot_live_chat_widget').contentFrame().getByRole('textbox', { name: 'Type your message' }).click();
+  await page.locator('#chatwoot_live_chat_widget').contentFrame().getByRole('textbox', { name: 'Type your message' }).fill(randomMessage);
+  await page.locator('#chatwoot_live_chat_widget').contentFrame().getByRole('button').nth(2).click();
+});
+
+test('Send an Attachment', async ({ page }) => {
+  await page.goto(baseURL);
+  await page.getByRole('button', { name: 'Open chat window' }).click();
+  await expect(page.locator('#chatwoot_live_chat_widget').contentFrame().getByRole('button')).toContainText('Start Conversation');
+  await page.locator('#chatwoot_live_chat_widget').contentFrame().getByRole('button', { name: 'Start Conversation' }).click();
+  await page.locator('#chatwoot_live_chat_widget').contentFrame().locator('label').click();
+  await page.locator('#chatwoot_live_chat_widget').contentFrame().getByLabel('', { exact: true }).setInputFiles('test.pdf');
+  await page.getByRole('button').click();
+});
---
+++ widget-host/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>C3Mailime Chat Widget Demo</h1>
+      <p>The chat widget should appear in the bottom right corner</p>
+    </div>
+  )
+}
+
+export default App
